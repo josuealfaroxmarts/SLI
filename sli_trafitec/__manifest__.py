bab2d6246f71e7bd7c1354628239ebdf9280629b
danielparraxmarts <daniel.parra.xmarts@gmail.com>
2021-06-30 16:41:36 -0400

    [CHANGE] Version manifest

--- sli_trafitec/__manifest__.py
+++ sli_trafitec/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'SLI',
-    'version': '10.0.411',
+    'version': '14.0.411',
 
     # any module necessary for this one to work correctly
     'depends': [
