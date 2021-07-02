5719dfc38649da5bd85dda431d344a363028a68d
danielparraxmarts <daniel.parra.xmarts@gmail.com>
2021-07-02 15:58:03 -0400

    [UPGRADE] Odoo v14

---
+++ SLl_TrafitecBusquedas/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+'name': 'SLI_TrafitecBusquedas',
+'version': '2.2',
+'description': 'Busquedas para Trafitec',
+'author': 'SOLUCIONES LOGISTICAS INTELIGENTES SA DE CV',
+'website': 'http://www.sli.mx',
+'category' : 'Utilerias',
+'summary' : '',
+'depends': ['base','sli_trafitec'],
+'data': [
+          'views/busqueda_viajes.xml',
+          'views/busqueda_descuentos.xml',
+          'views/busqueda_comisiones.xml',
+          'views/busqueda_cotizaciones.xml'
+         ],
+'demo': [],
+'installable': True,
+'auto_install': False,
+}
