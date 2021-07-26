110f159ad0f3ca629ad2c0f1ba10d94244f52a92
Daniel Parra <daniel.parra.xmarts@gmail.com>
2021-07-26 12:46:02 -0400

    [FIX] Evidences

--- api_fletex_connect/models/sync_data.py
+++ api_fletex_connect/models/sync_data.py
@@ -902,12 +902,12 @@ class SyncDataFletex(models.Model):
                     invoice = self.env['invoice.from.fletex'].search([
                             ('fletexShipmentReference', '=', shipment['shipment_id'])])
                     evidence_ids = self.env['trafitec.viajes.enviencias'].search([
-                            ('shipment_id', '=', shipment['shipment_id'])])
+                            ('linea_id', '=', shipment['shipment_id'])])
 
                     if not evidence_ids :
                         for evidence in shipment['evicences'] :
                             vals = {
-                                'shipment_id': shipment['shipment_id'],
+                                'linea_id': shipment['shipment_id'],
                                 'evidencia_file': evidence,
                                 'image_filename': "Evidencia de viaje {}".format(record['id']),
                                 'name': "Evidencia de viaje"
