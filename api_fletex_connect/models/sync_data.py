ecd38f32538d2de4a8c04adaa16c5f73f9fd634f
Daniel Parra <daniel.parra.xmarts@gmail.com>
2021-07-26 12:07:03 -0400

    [FIX] Boolean in users api

--- api_fletex_connect/models/sync_data.py
+++ api_fletex_connect/models/sync_data.py
@@ -34,7 +34,7 @@ class SyncDataFletex(models.Model):
             })
 
         # If the request brings users, the user manager is called
-        if len(users) > 0:
+        if users:
             for user in users['data']:
                 if user['role'] == 'driver':
                     self.res_partner_drivers_manager(user)
