a3761949f57d3a17e7dc3e1add857bf94e34dda4
Daniel Parra <daniel.parra.xmarts@gmail.com>
2021-07-27 12:33:54 -0400

    [FIX] Projects

--- api_fletex_connect/models/sync_data.py
+++ api_fletex_connect/models/sync_data.py
@@ -100,10 +100,9 @@ class SyncDataFletex(models.Model):
                 'params': {}
             })
         # If the request brings locations, the locations manager is called
-        if projects :
-            if len(projects['data']) > 0:
-                for project in projects['data']:
-                    self.projects_manager(project, headers)
+        if len(projects['data']) > 0:
+            for project in projects['data']:
+                self.projects_manager(project, headers)
 
         self.change_status_projects(headers)
 
